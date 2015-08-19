1fa98a758a7064734edcc7e542e08fe22cd52230
Migalin Daniil <midanil@microsoft.com>
2015-08-19 23:30:27 +0300

    Signal handling, more structure

--- butcher.py
+++ butcher.py
@@ -10,52 +10,10 @@ import readline
 import logging
 import re
 import atexit
+import signal
 
 logging.basicConfig(filename='/tmp/butcher.log', level=logging.DEBUG)
-
 BUTCHER_DIR = os.path.join(os.environ['HOME'], '.butcher')
-
-def load_hosts(env = None, cached=True):
-    result = []
-    cache_filename = os.path.join(BUTCHER_DIR, 'cache.json')
-    if not cached or not os.path.isfile(cache_filename):
-        if not os.path.isdir(BUTCHER_DIR):
-            os.mkdir(BUTCHER_DIR)
-        f = open(cache_filename, 'w')
-        cmd = shlex.split("knife search node '*' -a roles -a hostname -a chef_environment -F json")
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        data = json.load(p.stdout)
-        print "Loaded {} hosts".format(data['results'])
-        json.dump(data['rows'], f)
-        f.close()
-        result = data['rows']
-    else:
-        f = open(cache_filename, 'r')
-        result = json.load(f)
-
-    return result
-
-def filter_hosts(hosts, role=None, env=None):
-    logging.debug('filtering role %s env %s', role, env)
-    for host in (h for h in hosts if role in h['roles']):
-        if env == None or env == host['chef_environment']:
-            yield host
-
-def parse_and_run(command):
-    m = re.match("^\s*(?P<cmd>\w+)\s+%(?P<role>[-_A-Za-z0-9]+)(?:@(?P<env>[-_A-Za-z0-9]+))?\s*(?P<args>.+)*$", command)
-    if m:
-        filtered_hosts = list(filter_hosts(hosts, role=m.group('role'), env=m.group('env')))
-        if m.group('cmd') == 'hostlist':
-            for host in filtered_hosts:
-                print host['hostname']
-        elif m.group('cmd') == 'p_exec':
-            cmd = shlex.split("shmux -c '{}' -".format(m.group('args')))
-            logging.debug("Executing '%s' on %s", cmd, ','.join( (h['hostname'] for h in filtered_hosts) ))
-            p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-            ret = p.communicate(input='\n'.join((h['hostname'] for h in filtered_hosts)) + '\n' )
-            logging.debug("Return is %s", ret)
-    else:
-        print "Cannot parse command"
 
 class CommandCompleter(object):
 
@@ -99,26 +57,97 @@ class CommandCompleter(object):
             response = None
         return response
 
-hosts = load_hosts(cached=True)
-hostnames = [x['hostname'] for x in hosts]
-roles = set()
-envs = set()
-for host in hosts:
-    roles.update(['%' + role for role in host['roles']])
-    envs.add('@' + host['chef_environment'])
-commands = ['exec', 'p_exec', 'hostlist']
+class Butcher(object):
 
-readline.parse_and_bind('tab: complete')
-readline.set_completer(CommandCompleter(commands=commands, roles=roles, envs=envs).complete)
-readline.set_completer_delims(' ')
-histfile=os.path.join(BUTCHER_DIR, 'history')
-try:
-    readline.read_history_file(histfile)
-except IOError:
-    pass
-atexit.register(readline.write_history_file, histfile)
+    def __init__(self):
+        self.commands = ['exec', 'p_exec', 'hostlist', 'reload', 'threads', 'user']
+        self._shmux_running = False
+        self._load_hosts()
+        readline.parse_and_bind('tab: complete')
+        readline.set_completer(CommandCompleter(commands=self.commands, roles=self.roles, envs=self.envs).complete)
+        readline.set_completer_delims(' ')
+        histfile=os.path.join(BUTCHER_DIR, 'history')
+        try:
+            readline.read_history_file(histfile)
+        except IOError:
+            pass
+        atexit.register(readline.write_history_file, histfile)
+        signal.signal(signal.SIGINT, self._sigint())
 
-while True:
-    command = raw_input("> ")
-    parse_and_run(command)
 
+    def _sigint(self):
+        def __handler(signal, frame):
+            if self._shmux_running:
+                pass
+            else:
+                raise KeyboardInterrupt
+        return __handler
+
+    def _load_hosts(self, env = None, cached=True):
+        result = []
+        cache_filename = os.path.join(BUTCHER_DIR, 'cache.json')
+        if not cached or not os.path.isfile(cache_filename):
+            if not os.path.isdir(BUTCHER_DIR):
+                os.mkdir(BUTCHER_DIR)
+            f = open(cache_filename, 'w')
+            cmd = shlex.split("knife search node '*' -a roles -a hostname -a chef_environment -F json")
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            data = json.load(p.stdout)
+            print "Loaded {} hosts".format(data['results'])
+            json.dump(data['rows'], f)
+            f.close()
+            self.hosts = data['rows']
+        else:
+            f = open(cache_filename, 'r')
+            self.hosts = json.load(f)
+
+        self.roles = set()
+        self.envs = set()
+        for host in self.hosts:
+            self.roles.update(['%' + role for role in host['roles']])
+            self.envs.add('@' + host['chef_environment'])
+
+    def _filter_hosts(self, role=None, env=None):
+        logging.debug('filtering role %s env %s', role, env)
+        for host in (h for h in self.hosts if role in h['roles']):
+            if env == None or env == host['chef_environment']:
+                yield host
+
+    def _parse_and_run(self, command):
+        m = re.match("^\s*(?P<cmd>\w+)(?:\s+%(?P<role>[-_A-Za-z0-9]+)(?:@(?P<env>[-_A-Za-z0-9]+))?)?\s*(?P<args>.+)*$", command)
+        if m:
+            filtered_hosts = list(self._filter_hosts(role=m.group('role'), env=m.group('env')))
+            if m.group('cmd') == 'hostlist':
+                if not m.group('role'):
+                    print "USAGE: hostlist %WHAT[@WHERE]"
+                    return
+                for host in filtered_hosts:
+                    print host['hostname']
+            elif m.group('cmd') == 'p_exec':
+                if not m.group('role') or not m.group('args'):
+                    print "USAGE: p_exec %WHAT[@WHERE] COMMAND"
+                    return
+                cmd = shlex.split("shmux -c '{}' -".format(m.group('args')))
+                logging.debug("Executing '%s' on %s", cmd, ','.join( (h['hostname'] for h in filtered_hosts) ))
+                try:
+                    self._shmux_running = True
+                    p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+                    ret = p.communicate(input='\n'.join((h['hostname'] for h in filtered_hosts)) + '\n' )
+                finally:
+                    self._shmux_running = False
+        else:
+            print "Cannot parse command"
+
+    def run(self):
+        while True:
+            try:
+                command = raw_input("> ")
+                self._parse_and_run(command)
+            except KeyboardInterrupt:
+                print "\n"
+            except EOFError:
+                print "Bye!"
+                break
+
+if __name__ == '__main__':
+    Butcher().run()
